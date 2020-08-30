244cc53ff525cce39fa2b32af6f2600ba8f20e2f
Cesar <cbarcellig@gmail.com>
2020-08-30 18:17:30 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -14,10 +14,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//    urlDB = 'mongodb://localhost:27017/cafe';
-//} else {
-urlDB = 'mongodb+srv://cafe-user:Cesar123456@cluster0.tlk3m.mongodb.net/cafe'
-    //};
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://cafe-user:Cesar123456@cluster0.tlk3m.mongodb.net/cafe'
+};
 
 process.env.URLDB = urlDB;
