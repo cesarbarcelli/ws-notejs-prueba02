1909532f3cd80b4e2422c955e59831eb8f693152
Cesar <cbarcellig@gmail.com>
2020-08-30 18:33:18 -0500

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cafe-user:Cesar123456@cluster0.tlk3m.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URL;
 };
 
 process.env.URLDB = urlDB;
